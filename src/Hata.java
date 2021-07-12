14b2e82e814cbe1c2e729b38f8f8f21eab313a97
HellReys <wallpapertopia@gmail.com>
2021-07-12 15:40:49 +0300

    ilk sürüm

---
+++ src/Hata.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class Hata {
+  public static void main(String[] args) {
+   Scanner scanner = new Scanner(System.in);
+      System.out.print("Lütfen yaşınızı giriniz:");
+   int yas = scanner.nextInt();
+      System.out.println("Kullanıcının Yaşı: "+yas);
+      scanner.nextLine();
+      System.out.print("Lütfen Adınızı giriniz:");
+      String ad = scanner.nextLine();
+      System.out.println("Kullanıcının Adı: "+ad);
+  
+   
+      
+      
+      
+      
+      
+  }
+    
+}
